8ec0173bc0ef5075dfb12bbde5673cfe0f441899
Anthony Josh Moreno <anthonyjosh.moreno@dbtc-cebu.edu.ph>
2026-03-21 13:34:12 +0800

    Merge pull request #1 from IT211A-B/backend
    added missing information to entities

--- Backend/HuaSect_AMS_DBTCclasslib/Dtos/StudentDto.cs
+++ Backend/HuaSect_AMS_DBTCclasslib/Dtos/StudentDto.cs
@@ -2,10 +2,51 @@
 
 public class CreateStudentDto
 {
+    public string FirstName { get; set; } = "";
 
+    public string LastName { get; set; } = "";
+
+    public string MiddleName { get; set; } = "";
+
+    public string Suffix { get; set; } = "";
+
+    public string Email { get; set; } = "";
+
+    public int YearLevel { get; set; }
+}
+
+public class NewlyCreateStudentDto
+{
+    public int ID { get; set; }
+    
+    public string FirstName { get; set; } = "";
+
+    public string LastName { get; set; } = "";
+
+    public string MiddleName { get; set; } = "";
+
+    public string Suffix { get; set; } = "";
+
+    public string Email { get; set; } = "";
+
+    public string FullName { get; set; } = "";
+
+    public int YearLevel { get; set; }
 }
 
 public class UpdateStudentDto
 {
     public int ID { get; set; }
+
+    public string FirstName { get; set; } = "";
+
+    public string LastName { get; set; } = "";
+
+    public string MiddleName { get; set; } = "";
+
+    public string Suffix { get; set; } = "";
+
+    public string Email { get; set; } = "";
+
+    public int YearLevel { get; set; }
 }
--- Backend/HuaSect_AMS_DBTCclasslib/Migrations/20260321044411_AddedAttributesToCourseEntity.Designer.cs
+++ Backend/HuaSect_AMS_DBTCclasslib/Migrations/20260321044411_AddedAttributesToCourseEntity.Designer.cs
@@ -2,6 +2,7 @@
 using HuaSect_AMS_DBTCclasslib.DbCtx;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace HuaSect_AMS_DBTCclasslib.Migrations
 {
     [DbContext(typeof(DatabaseCtx))]
-    partial class DatabaseCtxModelSnapshot : ModelSnapshot
+    [Migration("20260321044411_AddedAttributesToCourseEntity")]
+    partial class AddedAttributesToCourseEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
