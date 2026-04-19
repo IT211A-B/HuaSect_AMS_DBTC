a94ab95a975ec7dd98550da4233ceaca26faeee2
invalid-code <jessgaming55@gmail.com>
2026-04-19 17:32:06 +0800

    fix: added protected fields from identity user and role because we can't use SkipEncryption attribute for these entities.

--- Backend/HuaSect_AMS_DBTCclasslib/Infrastructure/Extensions/EncryptionConventionExtensions.cs
+++ Backend/HuaSect_AMS_DBTCclasslib/Infrastructure/Extensions/EncryptionConventionExtensions.cs
@@ -8,6 +8,22 @@ public static class EncryptionConventionExtensions
     [AttributeUsage(AttributeTargets.Property)]
     public class SkipEncryptionAttribute : Attribute { }
 
+    private static readonly HashSet<string> ProtectedFields = new()
+    {
+        "Discriminator",
+        "PasswordHash",
+        "SecurityStamp",
+        "ConcurrencyStamp",
+        "NormalizedEmail",
+        "NormalizedUserName",
+        "Email",
+        "UserName",
+        "PhoneNumber",
+        "Name",
+        "NormalizedName"
+    };
+
+
     public static void AddGlobalStringEncryption(this ModelBuilder modelBuilder, IEncryptionService encryption)
     {
         foreach (var entityType in modelBuilder.Model.GetEntityTypes())
@@ -17,10 +33,12 @@ public static class EncryptionConventionExtensions
             foreach (var property in entityType.GetProperties())
             {
                 if (property.ClrType != typeof(string)) continue;
-                
+
                 if (property.GetValueConverter() != null) continue;
-                
+
                 if (property.IsPrimaryKey() || property.IsForeignKey()) continue;
+
+                if (ProtectedFields.Contains(property.Name)) continue;
 
                 var clrProperty = property.PropertyInfo;
                 if (clrProperty != null && clrProperty.GetCustomAttribute<SkipEncryptionAttribute>() != null)
