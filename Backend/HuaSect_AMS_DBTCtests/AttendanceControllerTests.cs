b8440f36839268c5be9029ae31771085f6bd9909
invalid-code <jessgaming55@gmail.com>
2026-04-26 20:34:39 +0800

    feat: added attendance controller unit test

---
+++ Backend/HuaSect_AMS_DBTCtests/AttendanceControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using Moq;
+using HuaSect_AMS_DBTC.Controllers;
+using HuaSect_AMS_DBTC.Service;
+
+namespace HuaSect_AMS_DBTCtests;
+
+[TestClass]
+public sealed class AttendanceControllerTests
+{
+    private Mock<IAttendanceService> _serviceMock;
+    private AttendanceController _controller;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _serviceMock = new Mock<IAttendanceService>();
+        _controller = new AttendanceController(_serviceMock.Object);
+    }
+    [TestMethod]
+    public void GetAllAttendanceRecordsTest()
+    {
+    }
+}
--- Backend/HuaSect_AMS_DBTCwebapi/Controllers/AuthController.cs
+++ Backend/HuaSect_AMS_DBTCwebapi/Controllers/AuthController.cs
@@ -191,10 +191,4 @@ namespace HuaSect_AMS_DBTC.Controllers
             return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
-
-    public class LoginDto
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
 }
