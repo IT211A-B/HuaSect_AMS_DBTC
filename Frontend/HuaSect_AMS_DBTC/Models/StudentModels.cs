b548a81fde389670b47b8db536dcf91be83a5313
mykyliecaballero <kylamarie.caballero@dbtc-cebu.edu.ph>
2026-05-13 12:07:57 +0800

    feat: finished implementing the models for the different pages for student controller

--- Frontend/HuaSect_AMS_DBTC/Models/StudentModels.cs
+++ Frontend/HuaSect_AMS_DBTC/Models/StudentModels.cs
@@ -4,6 +4,16 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HuaSect_AMS_DBTC.Models
-{
-    
+{   
+    public class StudentDashboardPage
+    {
+        public ICollection<Course> Courses { get; set; } = [];
+    }
+
+    public class AttendanceProfilePage
+    {
+        public required Course Course { get; set; }
+
+        public ICollection<Attendance> AttendanceRecord { get; set;} = [];
+    }
 }
